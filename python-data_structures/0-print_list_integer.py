b3a4aaabcf98fc1bbbd275c51bb878f5aa5c0fb3
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-03-14 20:28:27 +0400

    jhgfdsasdfgh

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-def print_list_integer(my_list=[]):
-    for i in my_list:
+def print_reversed_list_integer(my_list=[]):
+    # Siyahını tərsinə çeviririk və hər bir elementi 'i' olaraq götürürük
+    for i in my_list[::-1]:
         print("{:d}".format(i))
