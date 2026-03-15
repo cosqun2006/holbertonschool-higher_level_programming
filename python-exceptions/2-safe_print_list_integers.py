652773278fdabe2e9ad636952af2c47b7193ef2d
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-03-15 23:36:03 +0400

    jhgfdsasdfgfdgsedfsdhvfgh

--- python-exceptions/2-safe_print_list_integers.py
+++ python-exceptions/2-safe_print_list_integers.py
@@ -3,9 +3,12 @@ def safe_print_list_integers(my_list=[], x=0):
     count = 0
     for i in range(x):
         try:
-            print("{:d}".format(my_list[i]))
+            # Səhv burada idi: 'value' əvəzinə 'my_list[i]' yazmalısan
+            print("{:d}".format(my_list[i]), end="")
             count += 1
         except (ValueError, TypeError):
+            # Yalnız tip xətalarını tuturuq (stringləri keçmək üçün)
             pass
-    print("")
+
+    print("")  # Dövr bitəndə yeni sətir
     return count
