837ea5da7fcc90e7f6f3edbc8604e63793a37fb2
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-03-15 20:12:55 +0400

    jhgfdsasdfgfdggh

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    best_name = ""
     best_score = 0
     for key in a_dictionary:
         if a_dictionary[key] > best_score:
-            best_score = a_dictionary[key]
-    return best_score
+            best_name = key
+    return best_name
