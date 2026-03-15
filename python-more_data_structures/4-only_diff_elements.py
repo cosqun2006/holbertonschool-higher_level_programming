be98fb559f4ec60410793b68dca616353db4f3d9
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-03-15 18:41:21 +0400

    jhgfdsasdfgfdggh

---
+++ python-more_data_structures/4-only_diff_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def only_diff_elements(set_1, set_2):
+    new_list = []
+    set_1 = set(set_1)
+    set_2 = set(set_2)
+    for item in set_1:
+        if item in set_2:
+            new_list.append(item)
+    return new_list
