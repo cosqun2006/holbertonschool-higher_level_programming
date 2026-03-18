9853a2833ab5db55416e39aa94707ae4528a4c98
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-03-18 17:32:33 +0400

    Fix error message case and punctuation

--- python-test_driven_development/3-say_my_name.py
+++ python-test_driven_development/3-say_my_name.py
@@ -6,17 +6,11 @@ This module provides a function that prints a name.
 
 def say_my_name(first_name, last_name=""):
     """
-        Prints 'My name is <first name> <last name>'.
+    Prints 'My name is <first name> <last name>'.
+    """
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
 
-        Args:
-            first_name: The first name (string).
-            last_name: The last name (string, defaults to "").
-
-        Raises:
-            TypeError: If first_name or last_name are not strings.
-        """
-    if isinstance(first_name, str):
-        raise TypeError("first name must be a string.")
-    if isinstance(last_name, str):
-        raise TypeError("last name must be a string.")
     print("My name is {} {}".format(first_name, last_name))
