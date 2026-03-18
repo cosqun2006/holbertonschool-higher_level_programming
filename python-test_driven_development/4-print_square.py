4d91d89b752045f8fb1f18811ed32600a0a3b3f1
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-03-19 00:47:59 +0400

    Fix error message case andfdhdg punctuation

--- python-test_driven_development/4-print_square.py
+++ python-test_driven_development/4-print_square.py
@@ -1,10 +1,28 @@
 #!/usr/bin/python3
+"""
+Bu modul 'print_square' funksiyasını saxlayır.
+Modul sənədləşdirməsi boş olmamalı və mütləq burada olmalıdır.
+"""
+
+
 def print_square(size):
+    """
+    Bu funksiya '#' işarəsi ilə kvadrat çap edir.
+
+    Args:
+        size (int): Kvadratın bir tərəfinin uzunluğu.
+
+    Raises:
+        TypeError: Əgər size integer deyilsə.
+        ValueError: Əgər size 0-dan kiçikdirsə.
+    """
     if not isinstance(size, int):
+        if isinstance(size, float) and size < 0:
+            raise TypeError("size must be an integer")
         raise TypeError("size must be an integer")
+
     if size < 0:
         raise ValueError("size must be >= 0")
-    if size < 0 and isinstance(size, float):
-        raise TypeError("size must be an integer")
+
     for i in range(size):
         print("#" * size)
