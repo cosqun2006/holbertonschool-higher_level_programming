5221c347e3037a96289c64482d0caceb1bcfa1f7
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-03-19 17:46:37 +0400

    Fix error message case andfdhdg punctuation

---
+++ python-classes/3-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module that defines a Square class."""
+
+class Square:
+    """Square class."""
+    def __init__(self, size=0):
+        """Initialize the Square class."""
+        if not isinstance(size, int):
+            raise TypeError("Square size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.size = size
+    def area(self):
+        """Return the area of the square."""
+        return self.size ** 2
