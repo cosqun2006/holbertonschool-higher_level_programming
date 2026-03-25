d4f07a6a74ad43146f7f1af469567d5e9ae9e973
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-03-25 18:25:30 +0400

    Fix uation

---
+++ python-more_classes/6-rectangle.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python3
+"""
+Bu modul Rectangle klassını təyin edir.
+"""
+
+
+class Rectangle:
+    """Düzbucaqlı fiqurunu təmsil edən klass."""
+    # Public class attribute: Bütün obyektlər üçün ortaqdır
+    number_of_instances = 0
+
+    def __init__(self, width=0, height=0):
+        """Yeni bir Rectangle instansiyası yaradır."""
+        self.width = width
+        self.height = height
+        # Yeni obyekt yarandığı üçün klass atributunu artırırıq
+        Rectangle.number_of_instances += 1
+
+    @property
+    def width(self):
+        """Eni geri qaytarır."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Eni təyin edir."""
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """Hündürlüyü geri qaytarır."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Hündürlüyü təyin edir."""
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Düzbucaqlının sahəsini qaytarır."""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """Düzbucaqlının perimetrini qaytarır."""
+        if self.__width == 0 or self.__height == 0:
+            return 0
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        """Düzbucaqlını # simvolları ilə string kimi qaytarır."""
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        rect = []
+        for i in range(self.__height):
+            rect.append("#" * self.__width)
+        return "\n".join(rect)
+
+    def __repr__(self):
+        """Obyektin repr təmsilini qaytarır."""
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+
+    def __del__(self):
+        """Obyekt silinəndə mesaj çap edir."""
+        print("Bye rectangle...")
+        # Obyekt silindiyi üçün klass atributunu azaldırıq
+        Rectangle.number_of_instances -= 1
