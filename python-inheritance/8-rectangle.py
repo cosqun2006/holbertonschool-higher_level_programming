60102bdbadbc74fbf3f4db6518c48e9bf7741f81
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-03-27 03:50:10 +0400

    Fix uatifzgdfon

--- python-inheritance/8-rectangle.py
+++ python-inheritance/8-rectangle.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
-"""BaseGeometry modulu üçün sənədləşdirmə"""
-from wsgiref.validate import validator
-
+"""BaseGeometry-dən miras alan Rectangle modulu"""
 BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
+
 class Rectangle(BaseGeometry):
+    """Düzbucaqlı sinfi, BaseGeometry-dən miras alır"""
+
     def __init__(self, width, height):
         """Düzbucaqlını en və hündürlüklə inisializasiya edir.
 
@@ -12,7 +13,7 @@ class Rectangle(BaseGeometry):
             width (int): Düzbucaqlının eni.
             height (int): Düzbucaqlının hündürlüyü.
         """
-        self.integer-validator("width",width)
+        self.integer_validator("width", width)
         self.__width = width
-        self.integer-validator("height",height)
+        self.integer_validator("height", height)
         self.__height = height
