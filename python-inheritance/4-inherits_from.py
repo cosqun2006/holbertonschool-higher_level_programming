b3163d22e50d985c26ed67e96aeea6eb70363cb4
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-03-26 19:19:39 +0400

    Fix uation

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Obyektin dəqiq sinif yoxlanılması modulu"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    is_kind_of_classberhbfbrbgreng
+    jgnjdngb jhdfgndnjb ld;nhdfigjgbnfhdgijoknbf
+    """
+    return isinstance(obj, a_class) and type(obj) is not a_class
