5dda7c05030c7ce800e6c1fa59ef4819bf87f0df
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-03-26 18:29:28 +0400

    Fix uation

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python3
+"""
+Bu modul list-dən miras alan MyList sinfini təqdim edir.
+"""
+
+
 class MyList(list):
-    """Inherits from list"""
+    """
+    Standart list sinfinə print_sorted metodunu əlavə edən sinif.
+    """
+
     def print_sorted(self):
-        """Prints the list in ascending order"""
+        """
+        Siyahıdakı elementləri artan sıra ilə çap edir.
+        Orijinal siyahını dəyişmir.
+        """
         print(sorted(self))
