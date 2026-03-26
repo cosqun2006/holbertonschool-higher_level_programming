b3753651c1a12364ba933a0d24dc8a5a74eab5b7
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-03-26 19:20:19 +0400

    Fix uation

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,10 +1,16 @@
 #!/usr/bin/python3
-"""Obyektin dəqiq sinif yoxlanılması modulu"""
+"""Miras alma yoxlanılması modulu"""
 
 
-def is_kind_of_class(obj, a_class):
-    """
-    is_kind_of_classberhbfbrbgreng
-    jgnjdngb jhdfgndnjb ld;nhdfigjgbnfhdgijoknbf
+def inherits_from(obj, a_class):
+    """Obyektin verilmiş sinifdən (birbaşa və ya dolayısı ilə)
+    miras alıb-almadığını yoxlayır.
+
+    Args:
+        obj (any): Yoxlanılacaq obyekt.
+        a_class (type): Müqayisə ediləcək sinif.
+
+    Returns:
+        True əgər obyekt mirasçıdırsa və özü deyilsə; əks halda False.
     """
     return isinstance(obj, a_class) and type(obj) is not a_class
