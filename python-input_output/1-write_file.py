a1ed163ebf0b6658a1d2c019dd07225482616a42
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-03-28 02:22:26 +0400

    Fix uatifzgdfon

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module contains the function write_file
+"""
+
+
+def write_file(filename="", text=""):
+    """
+    jnhbgvfdghmj dhgahSH Dcbvgsaj dhchbvscgfnvsd
+    """
+    with open(filename, encoding="utf-8") as f:
+        f.write(text)
