717f8ef3722b54ca3f1bb6c3a717a215c5d834f0
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-03-28 03:05:00 +0400

    Fix uatifzgdfon

---
+++ python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Bu modul faylın sonuna mətn əlavə edən funksiyanı ehtiva edir
+"""
+
+
+def to_json_string(my_obj):
+    """
+    kjhgtjhsyhdgyewhfgrhmg
+    """
+    return json.dumps(my_obj)
