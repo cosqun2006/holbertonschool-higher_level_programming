6690158b18cce5d5359e5dbc927997d07443813d
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-03-27 19:05:39 +0400

    Fix uatifzgdfon

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -2,7 +2,7 @@
 import math
 from abc import ABC, abstractmethod
 
-
+# 1. Abstrakt ana sinif
 class Shape(ABC):
     @abstractmethod
     def area(self):
@@ -12,7 +12,7 @@ class Shape(ABC):
     def perimeter(self):
         pass
 
-
+# 2. Circle sinfi (Shape-in daxilində yox, kənarında olmalıdır)
 class Circle(Shape):
     def __init__(self, radius):
         self.radius = radius
@@ -23,7 +23,7 @@ class Circle(Shape):
     def perimeter(self):
         return 2 * math.pi * self.radius
 
-
+# 3. Rectangle sinfi
 class Rectangle(Shape):
     def __init__(self, width, height):
         self.width = width
@@ -35,7 +35,8 @@ class Rectangle(Shape):
     def perimeter(self):
         return 2 * (self.width + self.height)
 
-
+# 4. Duck Typing funksiyası
 def shape_info(obj):
+    # Obyektin növünü yoxlamırıq, sadəcə metodları çağırırıq
     print(f"Area: {obj.area()}")
     print(f"Perimeter: {obj.perimeter()}")
