67a39eceed4069ee42098c301def3dab7f8df920
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-03-28 02:32:30 +0400

    Fix uatifzgdfon

---
+++ python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+This module contains the function write_file
+"""
+
+def append_write(filename="", text=""):
+    """
+    jnhbgvfdghmj dhgahSH Dcbvgsaj dhchbvscgfnvsd
+    """
+    with open(filename, mode="w", encoding="utf-8") as f:
+        return f.append(text)
