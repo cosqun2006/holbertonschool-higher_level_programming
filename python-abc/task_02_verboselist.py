6b218f2055a221e08a914c65fd8606bb49a9a261
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-03-27 19:42:11 +0400

    Fix uatifzgdfon

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -1,24 +1,31 @@
 #!/usr/bin/python3
 """
-Module for Shape abstract class and its subclasses
+Module for VerboseList class that extends the built-in list
 """
 
 
 class VerboseList(list):
+    """A list that prints notifications when modified"""
+
     def append(self, item):
+        """Adds an item and prints a notification"""
         super().append(item)
-        print("Added [{}] to the list".format(item))
+        print("Added [{}] to the list.".format(item))
 
     def extend(self, iterable):
-        counts = len(iterable)
+        """Extends the list and prints how many items were added"""
+        items_count = len(iterable)
         super().extend(iterable)
-        print("Extended to the list with [{}] items".format(counts))
+        print("Extended the list with [{}] items.".format(items_count))
 
     def remove(self, item):
-        print("Removing [{}] from the list".format(item))
-        super().remove(self, item)
+        """Prints notification before removing an item"""
+        print("Removed [{}] from the list.".format(item))
+        super().remove(item)
 
-    def pop(self, index=- 1):
+    def pop(self, index=-1):
+        """Prints notification before popping an item"""
+        # Siyahı boş deyilsə, çıxacaq elementi əvvəlcədən tapırıq
         item = self[index]
-        print("Popped [{}] from the list".format(item))
+        print("Popped [{}] from the list.".format(item))
         return super().pop(index)
