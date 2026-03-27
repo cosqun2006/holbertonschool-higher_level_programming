58739ae9a23fcf37cae1e27335e8e9dbe28fd265
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-03-28 03:08:08 +0400

    Fix uatifzgdfon

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Bu modul obyekti JSON string-ə çevirən funksiyanı ehtiva edir
+"""
+import json
+
+
+def from_json_string(my_str):
+    """
+    kjsahgdhsfggrsdygfewvrsdtfghfdegrdfjhvbdrf
+    """
+    return json.loads(my_str)
