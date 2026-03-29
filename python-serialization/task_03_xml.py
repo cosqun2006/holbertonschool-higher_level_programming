6426148dd6d0e93c75ce4b537062c0bd1a81fe39
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-03-29 19:56:12 +0400

    Fix uatifzgdfon

--- python-serialization/task_03_xml.py
+++ python-serialization/task_03_xml.py
@@ -1,4 +1,6 @@
+import sys
 import xml.etree.ElementTree as ET
+from fileinput import filename
 
 
 def serialize_to_xml(dictionary, filename):
@@ -18,3 +20,16 @@ def serialize_to_xml(dictionary, filename):
 
     tree = ET.ElementTree(root)
     tree.write(filename, encoding="utf-8", xml_declaration=True)
+
+
+def deserialize_from_xml:
+    try:
+        tree = ET.parse(filename)
+        root = tree.getroot()
+
+        deserialed_dict = {}
+        for child in root:
+            deserialed_dict[child.tag] = child.text
+        return deserialed_dict
+    except FileNotFoundError:
+        return None
