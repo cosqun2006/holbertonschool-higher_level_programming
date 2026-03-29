b7ba55778de198a9888951ee42311779fb7ba7e0
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-03-29 18:18:55 +0400

    Fix uatifzgdfon

--- python-input_output/12-student.py
+++ python-input_output/12-student.py
@@ -1,26 +1,29 @@
 #!/usr/bin/python3
 """
-[1]
-[1,1]
-[1,2,1]
-[1,3,3,1]
-[1,4,6,4,1]
+Paskal üçbucağı modulu
 """
 
 
 def pascal_triangle(n):
+    """
+    n ölçülü Paskal üçbucağını qaytarır
+    """
     if n <= 0:
         return []
 
     triangle = [[1]]
 
     for i in range(1, n):
+
         row = [1]
 
         prev_row = triangle[i - 1]
 
         for j in range(1, i):
             row.append(prev_row[j - 1] + prev_row[j])
+
         row.append(1)
+
         triangle.append(row)
+
     return triangle
