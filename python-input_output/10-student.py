d953cd6c237aa0ed1669741f3a54ccadf0a82f23
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-03-29 17:24:07 +0400

    Fix uatifzgdfon

---
+++ python-input_output/10-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Tələbə (Student) sinfini təyin edən modul.
+"""
+
+
+class Student:
+    """
+    Tələbə məlumatlarını saxlayan sinif.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """Obyekti başladır"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Obyektin lüğətini qaytarır (Süzgəcləmə ilə).
+        """
+        # 1. Yoxlayırıq: attrs siyahıdır və içindəkilər stringdir?
+        if isinstance(attrs, list) and all(isinstance(a, str) for a in attrs):
+            yeni_luget = {}
+            for acat in attrs:
+                # Əgər obyektin daxilində (self.__dict__) belə bir açar varsa:
+                if acat in self.__dict__:
+                    yeni_luget[acat] = self.__dict__[acat]
+            return yeni_luget
+
+        # 2. Əgər attrs None-dırsa və ya siyahı deyilsə, hər şeyi qaytar
+        return self.__dict__
