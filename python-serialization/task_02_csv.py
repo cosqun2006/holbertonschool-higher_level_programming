e4fe4c5a79afd275e639025bcbf09982bfc69490
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-03-29 19:37:37 +0400

    Fix uatifzgdfon

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -4,16 +4,16 @@ import csv
 import json
 
 
-def convert_csv_to_json:
+def convert_csv_to_json(csv_filename):
     """
         CSV faylını oxuyur, hər sətri lüğətə (dict) çevirir
         və nəticəni JSON kimi saxlayır.
     """
     try:
-        with open('csv_filename', mode='r', encoding="utf-8") as csvfile:
+        with open(csv_filename, mode='r', encoding="utf-8") as csvfile:
             reader = csv.DictReader(csvfile)
             data_list = [row for row in reader]
-        with open('json.csv', mode='w', encoding="utf-8") as jsonfile:
+        with open('data.json', mode='w', encoding="utf-8") as jsonfile:
             json.dump(data_list, jsonfile, indent=4)
         return True
     except FileNotFoundError:
