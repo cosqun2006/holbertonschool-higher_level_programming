39ed8d96cb640b282bbf23448ea29d3b82078e70
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-04-10 16:09:11 +0000

    jhgfddhfgfrdjtfgyhj

--- SQL_more_queries/9-cities_by_state_join.sql
+++ SQL_more_queries/9-cities_by_state_join.sql
@@ -1,4 +1,4 @@
 -- uhyudsjdsugihjdskglsdzv
 SELECT cities.id, cities.name, states.name
-FROM cities JOIN states ON cities.state_id=state_id
+FROM cities JOIN states ON cities.state_id=states.id
 ORDER by cities.id ASC;
