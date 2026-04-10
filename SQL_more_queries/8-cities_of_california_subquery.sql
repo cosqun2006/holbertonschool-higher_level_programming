045afaf5a2df67211ef740c97ab59e523ffb7233
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-04-10 15:51:00 +0000

    jhgfddhfgfrdjtfgyhj

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,4 +1,10 @@
--- hygdstyfsjdfhsayigh
-SELECT id,name FROM cities WHERE state_id=(SELECT id FROM states WHERE_name='California');
+-- Lists all cities of California that can be found in the database hbtn_0d_usa
+SELECT id, name
+FROM cities
+WHERE state_id = (
+    SELECT id
+    FROM states
+    WHERE name = 'California'
+)
 ORDER BY id ASC;
 
