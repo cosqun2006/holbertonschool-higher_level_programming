8809f19c19447553c7ca013e6c238c79cf219aa5
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-04-11 20:10:22 +0400

    Fix uatifzgdfdsgxgfon

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-"""Script that displays all states where name matches the argument"""
+"""Script that lists all cities of a given state from the database"""
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
+    # Bazaya qoşulma
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -12,12 +13,20 @@ if __name__ == "__main__":
         db=sys.argv[3]
     )
     cursor = db.cursor()
-    cursor.execute("SELECT cities.id, cities.name, states.name FROM cities "
-                   "JOIN states ON cities.state_id = states.id "
-                   "WHERE states.name = %s ORDER BY cities.id ASC", (sys.argv[4],))
+
+    # SQL sorğusu - SQL Injection-a qarşı %s istifadə olunur
+    query = ("SELECT cities.id, cities.name, states.name FROM cities "
+             "JOIN states ON cities.state_id = states.id "
+             "WHERE states.name = BINARY %s "
+             "ORDER BY cities.id ASC")
+
+    # İkinci arqument olaraq axtarılan ştatın adını ötürürük
+    cursor.execute(query, (sys.argv[4],))
+
     rows = cursor.fetchall()
     for row in rows:
         print(row)
 
+    # Bağlantıları bağlayırıq
     cursor.close()
     db.close()
