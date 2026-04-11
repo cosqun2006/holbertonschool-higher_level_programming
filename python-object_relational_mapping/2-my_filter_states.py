80c1e3ac7e788f6ccb06eb80a9943ac5fd6165d3
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-04-11 19:12:36 +0400

    Fix uatifzgdfdsgxgfon

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-"""
-Script that displays all states where name matches
-the argument
-"""
-import sys
+"""Script that displays all states where name matches the argument"""
 import MySQLdb
+import sys
+
 
 if __name__ == "__main__":
-
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
@@ -16,10 +13,13 @@ if __name__ == "__main__":
         db=sys.argv[3]
     )
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}' ORDER BY id ASC".format(sys.argv[4]))
+    cursor.execute(
+        "SELECT * FROM states WHERE name = '{}' ORDER BY id ASC".format(
+            sys.argv[4]
+        )
+    )
     rows = cursor.fetchall()
     for row in rows:
         print(row)
-
     cursor.close()
     db.close()
