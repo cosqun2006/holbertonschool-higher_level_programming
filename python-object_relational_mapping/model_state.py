9f21a0d1d8a4625c9d52d8e1b6ba6c014e4d0d8f
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-04-11 20:26:10 +0400

    Fix uatifzgdfdsgxgfon

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -9,6 +9,9 @@ Base = declarative_base()
 
 
 class State(Base):
-    __tablename__ = "states"
+    """
+        State class represents the 'states' table in MySQL database
+    """
+    __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
