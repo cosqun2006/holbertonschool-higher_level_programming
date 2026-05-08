d13f007054c713e05131dc8cf65ce0522227d692
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-05-08 19:42:02 +0400

    Fix: Finalizide

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,24 +1,32 @@
 #!/usr/bin/python3
-"""Script that prints the State object with the name passed as argument"""
+"""
+'hbtn_0e_6_usa' verilənlər bazasına 'Louisiana' State obyektini əlavə edən skript
+"""
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
+from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
 if __name__ == "__main__":
-    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    engine = create_engine(
-        f"mysql+mysqldb://{username}{password}@localhost:3306/{db_name}"
+    if len(sys.argv) >= 4:
+        username = sys.argv[1]
+        password = sys.argv[2]
+        db_name = sys.argv[3]
 
-    )
-    Session = sessionmaker(bind=engine)
-    session = Session()
+        engine = create_engine(
+            'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+                username, password, db_name), pool_pre_ping=True
+        )
 
-    new_state = State(name="Lousiana")
+        Session = sessionmaker(bind=engine)
+        session = Session()
 
-    session.add(new_state)
-    session.commit()
+        new_state = State(name="Louisiana")
 
-    print(new_state.id)
-    session.close()
+        session.add(new_state)
+        session.commit()
+
+        print(new_state.id)
+
+        session.close()
