6660523e5e73b474a30214a70a91564d258373d9
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-05-08 19:40:20 +0400

    Fix: Finalizide

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 """Script that prints the State object with the name passed as argument"""
 import sys
-import sqlalchemy
-import create_engine
+from sqlalchemy import create_engine
 from sqlalchemy.orm import Session
-from model_state import Base, state
+from model_state import Base, State
 
 if __name__ == "__main__":
     username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
