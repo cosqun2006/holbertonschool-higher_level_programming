245e0c859c74ff94f7c5805fde26054e799772ea
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-05-08 18:38:04 +0400

    Fix: Finalizide

---
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Script that prints the State object with the name passed as argument"""
+import sys
+
+import
+from requests import Session..
+
+if __name__ == "__main__":
+    username, password, db_name = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    engine = create_engine(
+        f"mysql+mysqldb://{username}{password}@localhost:3306/{db_name}"
+
+    )
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name="Lousiana")
+
+    session.add(new_state)
+    session.commit()
+
+    print(new_state.id)
+    session.close
