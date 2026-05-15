e0e5bc8ebdcfa4490e9994d5807b599ef2f6e2a1
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-05-15 16:57:31 +0400

    Fix: Finalizide

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Script that prints the State object with the name passed as argument"""
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
