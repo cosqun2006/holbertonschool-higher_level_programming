63b92d73ac042515725e2c79d344f1dcb095685c
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-05-15 18:18:57 +0400

    Add tasks and set permissions

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""Script that changes the name of a State object from the database."""
-
+"""Contains the class definition of a City."""
 from sqlalchemy import Column, Integer, String, ForeignKey
 from model_state import Base
 
 
 class City(Base):
+    """City class that links to the cities table."""
     __tablename__ = 'cities'
-    id = Column(Integer, unique=True, nullable=False, primary_key=True)
+
+    id = Column(Integer, primary_key=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
-    state.id = Column(Integer, ForeignKey('states.id'), nullable=False)
+    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
