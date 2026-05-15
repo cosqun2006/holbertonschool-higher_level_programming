9903bb51c6990edce17911a7ecf8df6ed0762130
cosqun2006 <cosqunibrahimov500@gmail.com>
2026-05-15 17:36:24 +0400

    Add tasks and set permissions

---
+++ python-object_relational_mapping/13-model_state_delete.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Script that changes the name of a State object from the database"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True
+    )
+
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state_to_delete = session.query(State).filter(
+        State.name.like('%a%')
+    ).all()
+    for state in state_to_delete:
+        session.delete(state)
+    session.commit()
+    session.close()
